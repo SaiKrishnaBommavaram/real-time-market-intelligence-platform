fd161f40f0e2ef352c6f21b03f53da751464e6c8
Sai Krishna Bommavaram <sb8944@nyu.edu>
2026-05-31 23:41:22 -0500

    wire security middleware and tighten CORS policy
    wire security middleware and tighten CORS policy

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.config import settings
 from api.logging import configure_logging, log_request_middleware
 from api.routes.market import router as market_router
+from api.security import enforce_api_key_middleware, rate_limit_middleware
 
 
 configure_logging()
@@ -19,10 +20,12 @@ app.add_middleware(
     allow_origins=settings.allowed_origins,
     allow_origin_regex=settings.allowed_origin_regex,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET"],
+    allow_headers=["Content-Type", "X-API-Key", "X-Request-ID"],
 )
 
 app.middleware("http")(log_request_middleware)
+app.middleware("http")(rate_limit_middleware)
+app.middleware("http")(enforce_api_key_middleware)
 
 app.include_router(market_router)
